b7486a55630a4fbf3fb98dd2a0a839a705567010
Revanthbala24 <revanthbj451@gmail.com>
2025-06-14 13:23:44 +0530

    first commit of my project

---
+++ Frist.java
@@ -0,0 +1,8 @@
+public class Frist{
+    public static void main(String[]args){
+        System.out.println("revanth learning the git");
+        int a=10;
+        int b=20;
+        System.out.println(a+b);
+    }
+}
